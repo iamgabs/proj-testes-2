6f0c60fb09d46c54843a508c17505348ea3cc4de
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2024-09-16 02:07:33 -0300

    test: tests implementation

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from sub_tests import TestCaseSubtraction
 from division_tests import TestCaseDivision
 from multiplicacation_tests import TestCaseMultiplication
 from power_to_tests import TestCasePowerTo
+from read_xml_tests import TestCaseReadXml
 
 if __name__ == "__main__":
     unittest.main()
--- tests/division_tests.py
+++ tests/division_tests.py
@@ -1,7 +1,23 @@
 # imports
 import unittest
+from src.services.division import Dodivision as operation
 
 class TestCaseDivision(unittest.TestCase):
+    
+    def test_division_empty_list(self) -> None:
+        self.assertEqual(operation.do_division([]), 0)
 
-    def test_divide(self) -> None:
-        self.assertEqual(divide(), value) # type: ignore
+    def test_division_single_element(self) -> None:
+        self.assertEqual(operation.do_division([5]), 5)
+
+    def test_division_two_elements(self) -> None:
+        self.assertEqual(operation.do_division([4, 2]), 2)
+
+    def test_division_multiple_elements(self) -> None:
+        self.assertAlmostEqual(operation.do_division([1, 2, 3]), 0.16666666666)
+
+    def test_division_negative_numbers(self) -> None:
+        self.assertAlmostEqual(operation.do_division([-1, -2, -3]), -0.16666666666)
+
+    def test_division_mixed_numbers(self) -> None:
+        self.assertAlmostEqual(operation.do_division([1, -1, 2, -2, 3, -3]), -0.02777777777)
