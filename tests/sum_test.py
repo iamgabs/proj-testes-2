72b2afd8fed864f07ee6b10f1276e130790efb41
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2024-09-19 18:39:19 -0300

    fix: verificação melhor dos testes

--- tests/sum_test.py
+++ tests/sum_test.py
@@ -5,11 +5,22 @@ class TestCaseSum(unittest.TestCase):
     
     def setUp(self):
         self.sum_operations = [
-            ('soma', ['10 20', '-5 (-15)', '-1 3', '-18 0', '2.5 4.3'], [30.0, -20.0, 2.0, -18.0, 6.8])
+            (
+                'soma', 
+                ['10 20', '-5 (-15)', '-1 3', '-18 0', '2.5 4.3'], 
+                ['30', '-20', '2', '-18', '6,8']
+            )
         ]
     
     def test_sum_operations(self) -> None:
         """
         Test for each sum operation in the cases.
         """
-        execute_operations(self.sum_operations)
+        obtained_results = execute_operations(self.sum_operations)
+
+        print(f'Resultados obtidos: {obtained_results}')
+        
+        expected_results = self.sum_operations[0][2]
+
+        for obtained, expected in zip(obtained_results, expected_results):
+            self.assertEqual(obtained, expected)
