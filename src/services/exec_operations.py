1619d13f72e98a6ad8f28f8383d75c5317e6f187
Amonel Leais <96805614+Amoneleais@users.noreply.github.com>
2024-09-19 18:39:05 -0300

    fix: função agora retorna os valores para testes

--- src/services/exec_operations.py
+++ src/services/exec_operations.py
@@ -19,6 +19,8 @@ def execute_operations(operations: List[Tuple[str, List[str], List[float]]]):
 
     driver.get('https://www.calculadoraonline.com.br/basica')
 
+    real_results = []
+
     for procedimento_desc, expressions, results in operations:
         print(f'\n** TESTANDO OPERAÇÃO: {procedimento_desc} **')
 
@@ -38,6 +40,8 @@ def execute_operations(operations: List[Tuple[str, List[str], List[float]]]):
             time.sleep(1.5)
 
             real_result = input_field.get_attribute("value")
+            real_results.append(real_result)
             print(f'--> RESULTADO OBTIDO: {real_result}\n')
 
     driver.quit()
+    return real_results
